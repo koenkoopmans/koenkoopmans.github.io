06df4fc652c460e1a810a2dcb902e26a2e03a0d8
koenkoopmans <koen.koopmans@ictmbo.nl>
2018-05-16 10:48:48 +0200

    sdf

--- sw.js
+++ sw.js
@@ -14,3 +14,37 @@ self.addEventListener('install', function(event) {
             })
     );
 });
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
+
+
+
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
